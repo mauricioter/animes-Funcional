3b5d0217afa231e01f728396d32b5780dd38e71b
mauricioter <Mzueiro123@gmail.com>
2023-08-17 16:14:49 -0300

    Funcao-de-busca

--- App.tsx
+++ App.tsx
@@ -1,32 +1,50 @@
 import { ScrollView, Text, View } from "react-native";
 import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import HomeAnime from "./src/home/homeAnime";
+import configuracao from "./src/home/configuracao";
 import Home from "./src/home";
-import { Colors } from "react-native/Libraries/NewAppScreen";
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function Tabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Animes" component={Home} options={{ headerShown: false }} />
+      <Tab.Screen name="Config" component={configuracao} options={{ headerShown: false }} />
+    </Tab.Navigator>
+  )
+}
+
 
 export default function App() {
   return (
-    <ScrollView>
-      <View>
-        <View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Animes Family" component={Tabs} options={{ headerShown: false }} />
+        <Stack.Screen name="Config" component={configuracao} options={{ headerShown: false }} />
+      </Stack.Navigator>
+      {/* <ScrollView style={{ backgroundColor: "#2F4F4F" }}>
           <Text
             style={style.textTop}
           >Anime Family</Text>
           <Home></Home>
-        </View>
+        </ScrollView>
         <View>
-          <HomeAnime></HomeAnime>
-        </View>
-      </View>
-    </ScrollView>
+        </View> */}
+    </NavigationContainer>
   )
 }
 
 const style = StyleSheet.create({
   textTop: {
-    fontSize: 25,
-    margin: 20,
+    fontSize: 28,
+    marginHorizontal: 20,
+    marginTop: 10,
     fontWeight: "bold",
+    color: "#000"
   }
 })
--- src/home/index.js
+++ src/home/index.js
@@ -1,22 +1,89 @@
-import React from "react";
-import { Button, TextInput, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
+import { Button, TextInput, TouchableOpacity, View, FlatList, Image, Text } from "react-native";
 import Icon from 'react-native-vector-icons/FontAwesome5';
+import Animes from "../../animes";
 
 export default function Home() {
+    // fazer uma funcao que vai fazer a busca dp texto do input 
+    // primeira const que vai ficar responsavel pelo o texto do Input 
+    const [busca, setBusca] = useState('')
+    //  segundo const: vai ficar responsavel pela  busca do Anime 
+    const [filterAnime, setFilterAnime] = useState(Animes)
+
+    // const responsavel por filtrar o nome do anime e trazer somente aquele que eu pedi
+    const animesFiltter = (text) => {
+        setBusca(text);
+        const filtrar = Animes.filter(item => item.title.romaji.toLowerCase().includes(text.toLowerCase())
+        );
+        //  SetFilterAnime virou filtrar 
+        setFilterAnime(filtrar)
+    }
     return (
-        <View style={{ backgroundColor: "#848482", borderRadius: 16, marginHorizontal: 20, margin: 15, paddingHorizontal: 20, flexDirection: "row", alignItems: "center", justifyContent: "space-between" }} >
-            <TextInput
-                onChangeText={0}
-                placeholder="Pesquisa..."
-
-            >
-            </TextInput>
-            <TouchableOpacity style={{ alignItems: "flex-end" }}>
-                <Icon name="search" solid
+        <View >
+            <Text style={{ fontSize: 26, fontWeight: "bold", paddingHorizontal: 20, paddingTop: 15, color: "#000" }}>Anime Family</Text>
+            <View style={{ backgroundColor: "#848482", borderRadius: 16, marginHorizontal: 20, marginTop: 15, paddingHorizontal: 20, flexDirection: "row", alignItems: "center", justifyContent: "space-between", marginBottom: 15 }} >
+                <TextInput
+                    onChangeText={animesFiltter}
+                    placeholder="Pesquisa..."
+                    autoComplete="additional-name"
+                    value={busca}
                 >
-                </Icon>
-
-            </TouchableOpacity>
-        </View >
+                </TextInput>
+                <TouchableOpacity
+                    onPress={() => TextInput}
+                    style={{ alignItems: "flex-end" }}>
+                    <Icon name="search" solid
+                    >
+                    </Icon>
+                </TouchableOpacity>
+            </View>
+            <View style={{ flexDirection: "row", flexWrap: "wrap", justifyContent: "space-between", paddingHorizontal: 20, paddingTop: 5, backgroundColor: "white" }}>
+                <FlatList numColumns={2}
+                    data={filterAnime}
+                    keyExtractor={(item) => item.id}
+                    renderItem={({ item }) =>
+                        <TouchableOpacity
+                            onPress={() => console.warn('olaa')}
+                            style={{ justifyContent: "center", alignItems: "center", maxWidth: 175, height: 250, marginHorizontal: 10 }} key={item.id}>
+                            <Image
+                                style={{ height: 200, width: 150, borderRadius: 8 }}
+                                source={{
+                                    uri: item.coverImage.large
+                                }}
+                            />
+                            <Text>
+                                {item.title.romaji}
+                            </Text>
+                        </TouchableOpacity>
+                    }
+                />
+            </View >
+        </View>
     )
 }
+
+
+
+
+// <View
+// >
+// <FlatList numColumns={2}
+//     data={Animes}
+//     keyExtractor={(item) => item.id}
+//     renderItem={({ item }) =>
+//         <TouchableOpacity
+//             onPress={() => console.warn('olaa')}
+//             style={{ justifyContent: "center", alignItems: "center", maxWidth: 175, height: 250, marginHorizontal: 10 }} key={item.id}>
+//             <Image
+//                 style={{ height: 200, width: 150, borderRadius: 8 }}
+//                 source={{
+//                     uri: item.coverImage.large
+//                 }}
+//             />
+//             <Text>
+//                 {item.title.romaji}
+//             </Text>
+//         </TouchableOpacity>
+//     }
+// />
+// </View>
